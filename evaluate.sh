26155ace6a5d454fbcc975d40ee250d5e98535f8
tiarrrrrr <102753752+tiarrrrrr@users.noreply.github.com>
2023-05-03 20:02:14 -0700

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,20 +1,20 @@
-echo "---------ashaninka---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/ashaninka/pretrain_result.txt  --gold_reference ./processed_data/ashaninka/dev.es >> result_new.txt
-echo "---------aymara---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/aymara/pretrain_result.txt  --gold_reference ./processed_data/aymara/dev.es >> result_new.txt
-echo "---------bribri---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/bribri/pretrain_result.txt  --gold_reference ./processed_data/bribri/dev.es >> result_new.txt
-echo "---------guarani---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/guarani/pretrain_result.txt  --gold_reference ./processed_data/guarani/dev.es >> result_new.txt
-echo "---------hñähñu---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/hñähñu/pretrain_result.txt  --gold_reference ./processed_data/hñähñu/dev.es >> result_new.txt
-echo "---------nahuatl---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/nahuatl/pretrain_result.txt  --gold_reference ./processed_data/nahuatl/dev.es >> result_new.txt
-echo "---------quechua---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/quechua/pretrain_result.txt  --gold_reference ./processed_data/quechua/dev.es >> result_new.txt
-echo "---------raramuri---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/raramuri/pretrain_result.txt  --gold_reference ./processed_data/raramuri/dev.es >> result_new.txt
-echo "---------shipibo_konibo---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/shipibo_konibo/pretrain_result.txt  --gold_reference ./processed_data/shipibo_konibo/dev.es >> result_new.txt
-echo "---------wixarika---------" >> result_new.txt
-python3 evaluate.py --system_output ./processed_data/wixarika/pretrain_result.txt  --gold_reference ./processed_data/wixarika/dev.es >> result_new.txt
+echo "---------ashaninka---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/ashaninka/finetune_result.txt  --gold_reference ./processed_data/ashaninka/dev.es >> result_finetune.txt
+echo "---------aymara---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/aymara/finetune_result.txt  --gold_reference ./processed_data/aymara/dev.es >> result_finetune.txt
+echo "---------bribri---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/bribri/finetune_result.txt  --gold_reference ./processed_data/bribri/dev.es >> result_finetune.txt
+echo "---------guarani---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/guarani/finetune_result.txt  --gold_reference ./processed_data/guarani/dev.es >> result_finetune.txt
+echo "---------hñähñu---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/hñähñu/finetune_result.txt  --gold_reference ./processed_data/hñähñu/dev.es >> result_finetune.txt
+echo "---------nahuatl---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/nahuatl/finetune_result.txt  --gold_reference ./processed_data/nahuatl/dev.es >> result_finetune.txt
+echo "---------quechua---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/quechua/finetune_result.txt  --gold_reference ./processed_data/quechua/dev.es >> result_finetune.txt
+echo "---------raramuri---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/raramuri/finetune_result.txt  --gold_reference ./processed_data/raramuri/dev.es >> result_finetune.txt
+echo "---------shipibo_konibo---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/shipibo_konibo/finetune_result.txt  --gold_reference ./processed_data/shipibo_konibo/dev.es >> result_finetune.txt
+echo "---------wixarika---------" >> result_finetune.txt
+python3 evaluate_result.py --system_output ./processed_data/wixarika/finetune_result.txt  --gold_reference ./processed_data/wixarika/dev.es >> result_finetune.txt
